dc6d8cddd63f7b01f2736c4a3cced5cb05eeaeba
D <bieler15147@live.de>
2026-05-24 15:15:58 +0200

    feat: Add DynamicDialogDrawer component with responsive behavior and stories

---
+++ src/components/DynamicDialogDrawer.tsx
@@ -0,0 +1,344 @@
+"use client";
+
+import * as React from "react";
+import { cn } from "../utils/cn";
+import { Button } from "./Button";
+import {
+	Dialog,
+	DialogClose,
+	DialogContent,
+	DialogDescription,
+	DialogFooter,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from "./Dialog";
+import {
+	Drawer,
+	DrawerClose,
+	DrawerContent,
+	DrawerDescription,
+	DrawerFooter,
+	DrawerHeader,
+	DrawerTitle,
+	DrawerTrigger,
+} from "./Drawer";
+import { useIsMobile } from "../hooks/useMobile";
+
+type DialogProps = React.ComponentProps<typeof Dialog>;
+type DialogTriggerProps = React.ComponentProps<typeof DialogTrigger>;
+type DialogCloseProps = React.ComponentProps<typeof DialogClose>;
+type DialogContentProps = React.ComponentProps<typeof DialogContent>;
+type DialogFooterProps = React.ComponentProps<typeof DialogFooter>;
+
+type DrawerProps = React.ComponentProps<typeof Drawer>;
+type DrawerTriggerProps = React.ComponentProps<typeof DrawerTrigger>;
+type DrawerCloseProps = React.ComponentProps<typeof DrawerClose>;
+type DrawerContentProps = React.ComponentProps<typeof DrawerContent>;
+
+type DynamicDialogDrawerProps = DialogProps &
+	Partial<DrawerProps> & {
+		direction?: DrawerProps["direction"];
+	};
+
+type DynamicDialogDrawerTriggerProps = Omit<DialogTriggerProps, "render"> &
+	Omit<DrawerTriggerProps, "asChild"> & {
+		asChild?: DrawerTriggerProps["asChild"];
+		render?: DialogTriggerProps["render"];
+	};
+
+type DynamicDialogDrawerCloseProps = Omit<DialogCloseProps, "render"> &
+	Omit<DrawerCloseProps, "asChild"> & {
+		asChild?: DrawerCloseProps["asChild"];
+		render?: DialogCloseProps["render"];
+	};
+
+type DynamicDialogDrawerContentProps = DialogContentProps &
+	DrawerContentProps & {
+		dialogClassName?: string;
+		drawerClassName?: string;
+	};
+
+const DynamicDialogDrawerContext = React.createContext(false);
+
+function useDynamicDialogDrawer() {
+	return React.useContext(DynamicDialogDrawerContext);
+}
+
+function DynamicDialogDrawer({
+	activeSnapPoint,
+	autoFocus,
+	closeThreshold,
+	container,
+	defaultOpen,
+	dismissible,
+	direction = "bottom",
+	disablePreventScroll,
+	fadeFromIndex,
+	fixed,
+	handleOnly,
+	modal,
+	nested,
+	noBodyStyles,
+	onAnimationEnd,
+	onClose,
+	onDrag,
+	onOpenChange,
+	onRelease,
+	open,
+	preventScrollRestoration,
+	repositionInputs,
+	scrollLockTimeout,
+	setActiveSnapPoint,
+	setBackgroundColorOnScale,
+	shouldScaleBackground,
+	snapPoints,
+	snapToSequentialPoint,
+	...dialogProps
+}: DynamicDialogDrawerProps) {
+	const isMobile = useIsMobile();
+
+	if (isMobile) {
+		const drawerProps = {
+			activeSnapPoint,
+			autoFocus,
+			children: dialogProps.children,
+			closeThreshold,
+			container,
+			defaultOpen,
+			dismissible,
+			direction,
+			disablePreventScroll,
+			fadeFromIndex,
+			fixed,
+			handleOnly,
+			modal,
+			nested,
+			noBodyStyles,
+			onAnimationEnd,
+			onClose,
+			onDrag,
+			onOpenChange,
+			onRelease,
+			open,
+			preventScrollRestoration,
+			repositionInputs,
+			scrollLockTimeout,
+			setActiveSnapPoint,
+			setBackgroundColorOnScale,
+			shouldScaleBackground,
+			snapPoints,
+			snapToSequentialPoint,
+		} as DrawerProps;
+
+		return (
+			<DynamicDialogDrawerContext.Provider value={isMobile}>
+				<Drawer {...drawerProps} />
+			</DynamicDialogDrawerContext.Provider>
+		);
+	}
+
+	return (
+		<DynamicDialogDrawerContext.Provider value={isMobile}>
+			<Dialog
+				{...dialogProps}
+				defaultOpen={defaultOpen}
+				disablePointerDismissal={
+					dialogProps.disablePointerDismissal ?? dismissible === false
+				}
+				modal={modal as DialogProps["modal"]}
+				onOpenChange={onOpenChange as DialogProps["onOpenChange"]}
+				open={open}
+			/>
+		</DynamicDialogDrawerContext.Provider>
+	);
+}
+
+function DynamicDialogDrawerTrigger({
+	asChild,
+	render,
+	children,
+	...props
+}: DynamicDialogDrawerTriggerProps) {
+	const isMobile = useDynamicDialogDrawer();
+
+	if (isMobile) {
+		if (React.isValidElement(render)) {
+			return (
+				<DrawerTrigger
+					asChild
+					{...(props as React.ComponentProps<typeof DrawerTrigger>)}
+				>
+					{render}
+				</DrawerTrigger>
+			);
+		}
+
+		return (
+			<DrawerTrigger
+				asChild={asChild}
+				{...(props as React.ComponentProps<typeof DrawerTrigger>)}
+			>
+				{children}
+			</DrawerTrigger>
+		);
+	}
+
+	if (asChild && React.isValidElement(children)) {
+		return (
+			<DialogTrigger
+				render={children}
+				{...(props as React.ComponentProps<typeof DialogTrigger>)}
+			/>
+		);
+	}
+
+	return (
+		<DialogTrigger
+			render={render}
+			{...(props as React.ComponentProps<typeof DialogTrigger>)}
+		>
+			{children}
+		</DialogTrigger>
+	);
+}
+
+function DynamicDialogDrawerClose({
+	asChild,
+	render,
+	children,
+	...props
+}: DynamicDialogDrawerCloseProps) {
+	const isMobile = useDynamicDialogDrawer();
+
+	if (isMobile) {
+		if (React.isValidElement(render)) {
+			return (
+				<DrawerClose
+					asChild
+					{...(props as React.ComponentProps<typeof DrawerClose>)}
+				>
+					{render}
+				</DrawerClose>
+			);
+		}
+
+		return (
+			<DrawerClose
+				asChild={asChild}
+				{...(props as React.ComponentProps<typeof DrawerClose>)}
+			>
+				{children}
+			</DrawerClose>
+		);
+	}
+
+	if (asChild && React.isValidElement(children)) {
+		return (
+			<DialogClose
+				render={children}
+				{...(props as React.ComponentProps<typeof DialogClose>)}
+			/>
+		);
+	}
+
+	return (
+		<DialogClose
+			render={render}
+			{...(props as React.ComponentProps<typeof DialogClose>)}
+		>
+			{children}
+		</DialogClose>
+	);
+}
+
+function DynamicDialogDrawerContent({
+	className,
+	dialogClassName,
+	drawerClassName,
+	showCloseButton = true,
+	...props
+}: DynamicDialogDrawerContentProps) {
+	const isMobile = useDynamicDialogDrawer();
+
+	if (isMobile) {
+		return (
+			<DrawerContent
+				className={cn(className, drawerClassName)}
+				{...(props as React.ComponentProps<typeof DrawerContent>)}
+			/>
+		);
+	}
+
+	return (
+		<DialogContent
+			className={cn(className, dialogClassName)}
+			showCloseButton={showCloseButton}
+			{...(props as React.ComponentProps<typeof DialogContent>)}
+		/>
+	);
+}
+
+function DynamicDialogDrawerHeader(props: React.ComponentProps<"div">) {
+	const isMobile = useDynamicDialogDrawer();
+	return isMobile ? <DrawerHeader {...props} /> : <DialogHeader {...props} />;
+}
+
+function DynamicDialogDrawerFooter({
+	showCloseButton = false,
+	children,
+	...props
+}: React.ComponentProps<"div"> & Pick<DialogFooterProps, "showCloseButton">) {
+	const isMobile = useDynamicDialogDrawer();
+
+	if (isMobile) {
+		return (
+			<DrawerFooter {...props}>
+				{children}
+				{showCloseButton && (
+					<DrawerClose asChild>
+						<Button variant="outline">Close</Button>
+					</DrawerClose>
+				)}
+			</DrawerFooter>
+		);
+	}
+
+	return (
+		<DialogFooter showCloseButton={showCloseButton} {...props}>
+			{children}
+		</DialogFooter>
+	);
+}
+
+function DynamicDialogDrawerTitle(
+	props: React.ComponentProps<typeof DialogTitle> &
+		React.ComponentProps<typeof DrawerTitle>,
+) {
+	const isMobile = useDynamicDialogDrawer();
+	return isMobile ? <DrawerTitle {...props} /> : <DialogTitle {...props} />;
+}
+
+function DynamicDialogDrawerDescription(
+	props: React.ComponentProps<typeof DialogDescription> &
+		React.ComponentProps<typeof DrawerDescription>,
+) {
+	const isMobile = useDynamicDialogDrawer();
+
+	return isMobile ? (
+		<DrawerDescription {...props} />
+	) : (
+		<DialogDescription {...props} />
+	);
+}
+
+export {
+	DynamicDialogDrawer,
+	DynamicDialogDrawerClose,
+	DynamicDialogDrawerContent,
+	DynamicDialogDrawerDescription,
+	DynamicDialogDrawerFooter,
+	DynamicDialogDrawerHeader,
+	DynamicDialogDrawerTitle,
+	DynamicDialogDrawerTrigger,
+};
--- src/index.ts
+++ src/index.ts
@@ -11,6 +11,7 @@ export * from "./components/Confirm";
 export * from "./components/CopyButton";
 export * from "./components/DataTable";
 export * from "./components/Dialog";
+export * from "./components/DynamicDialogDrawer";
 export * from "./components/FloatingActionButton";
 export * from "./components/FormBuilder";
 export * from "./components/GenUI";
