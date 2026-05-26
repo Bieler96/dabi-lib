aa6fbd11a2facc660a74f67fdd56257529164b39
D <bieler15147@live.de>
2026-05-26 18:44:56 +0200

    feat: Add Chat component with message styling and storybook examples

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ export * from "./components/Button";
 export * from "./components/ButtonGroup";
 export * from "./components/Card";
 export * from "./components/Checkbox";
+export * from "./components/Chat";
 export * from "./components/Chip";
 export * from "./components/Confirm";
 export * from "./components/CopyButton";
